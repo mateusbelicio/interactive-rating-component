4a8d7540dafb2db6becf8b603e232530f796dd38
Mateus Belicio <mateusbelicio@hotmail.com>
2022-11-17 08:04:59 -0300

    Update solution

--- script.js
+++ script.js
@@ -1,21 +1,15 @@
 'use strict';
 
-const options = [...document.querySelectorAll('.c-input-container input')];
-const button = document.querySelector('.c-button');
+const form = document.querySelector('.c-form');
 const ratingCard = document.getElementById('rating-state');
 const thanksCard = document.getElementById('thanks-state');
 const userChoice = document.querySelector('.i-user-choice');
 
-// Handler functions
-button.addEventListener('click', (e) => {
+// Event handler
+form.addEventListener('submit', (e) => {
   e.preventDefault();
 
-  const isSelected = options.some((op) => op.checked === true);
-
-  if (!isSelected) return;
-
-  const optionSelected = options.find((op) => op.checked === true);
-  userChoice.textContent = optionSelected.id.split('--')[1];
+  userChoice.textContent = form.querySelector('input[type="radio"]:checked').value;
 
   ratingCard.classList.add('hidden');
   thanksCard.classList.remove('hidden');
